d640809da7115b4cb6e59177e8e33b82eac83e30
alireza-kahdooee <alireza.kahdooee@hotmail.com>
2020-11-18 23:30:48 +0330

    Install laravel/ui, npm install && npm run dev(issue #3).

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('organization.user.login-employee');
 });
+
+/**
+ *
+ */
+Route::get('/clear-cache', function () {
+//    $exitCode = Artisan::call('optimize');
+    $exitCode = Artisan::call('optimize');
+    $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('view:clear');
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('config:cache');
+    $exitCode = Artisan::call('clear-compiled');
+    return $exitCode;
+    return 'DONE'; //Return anything
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
